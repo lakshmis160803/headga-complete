e4338c5ccf446740794b0de85ea17239f30141c4
Lakshmi S <lakshmistla17@gmail.com>
2026-06-04 18:43:08 +0530

    fix multer-storage-cloudinary dependency

--- SERVER/routes/authRoutes.js
+++ SERVER/routes/authRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import passport from "../controllers/passport.js";
 
-import { updateProfile } from "../controllers/authController.js";
+// import { updateProfile } from "../controllers/authController.js";
 import { upload } from "../utils/cloudinary.js";
 import {
   preRegister,
@@ -67,6 +67,6 @@ router.get(
   getMe
 );
 
-router.put("/profile", protect, updateProfile);
+// router.put("/profile", protect, updateProfile);
 router.put("/profile", protect, upload.single("avatar"), updateProfile);
 export default router;
