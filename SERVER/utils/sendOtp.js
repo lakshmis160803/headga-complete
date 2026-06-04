1c2e08dee8e755f26f1bac9a25beca68c98b6ad7
Lakshmi S <lakshmistla17@gmail.com>
2026-06-04 15:43:56 +0530

    fix sendotp code

--- SERVER/utils/sendOtp.js
+++ SERVER/utils/sendOtp.js
@@ -1,25 +1,41 @@
 import nodemailer from "nodemailer";
 
 export const sendOtpEmail = async (to, otp) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp-relay.brevo.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: process.env.BREVO_USER,
-      pass: process.env.BREVO_PASS,
-    },
-  });
+  try {
+    console.log("CREATING TRANSPORT");
 
-  const info = await transporter.sendMail({
-    from: '"Headga" <lakshmistla17@gmail.com>',
-    to,
-    subject: "Your OTP Code",
-    html: `
-      <h2>Email Verification</h2>
-      <h1>${otp}</h1>
-    `,
-  });
+    const transporter = nodemailer.createTransport({
+      host: "smtp-relay.brevo.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.BREVO_USER,
+        pass: process.env.BREVO_PASS,
+      },
+    });
 
-  console.log(info.messageId);
+    console.log("VERIFYING SMTP");
+
+    await transporter.verify();
+
+    console.log("SMTP VERIFIED");
+
+    console.log("SENDING EMAIL");
+
+    const info = await transporter.sendMail({
+      from: '"Headga" <lakshmistla17@gmail.com>',
+      to,
+      subject: "Your OTP Code",
+      html: `
+        <h2>Email Verification</h2>
+        <h1>${otp}</h1>
+      `,
+    });
+
+    console.log("EMAIL SENT");
+    console.log(info);
+  } catch (err) {
+    console.error("BREVO ERROR:", err);
+    throw err;
+  }
 };
