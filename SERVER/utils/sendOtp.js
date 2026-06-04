757257e90f66e755a0145eacd72a93e8f6bf19a8
Lakshmi S <lakshmistla17@gmail.com>
2026-06-04 15:04:21 +0530

    fix sendotp cpde

--- SERVER/utils/sendOtp.js
+++ SERVER/utils/sendOtp.js
@@ -8,14 +8,20 @@ export const sendOtpEmail = async (to, otp) => {
   );
 
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    service: "gmail",
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
   });
+
+  try {
+    await transporter.verify();
+    console.log("SMTP VERIFIED");
+  } catch (err) {
+    console.error("SMTP ERROR:", err);
+    throw err;
+  }
 
   await transporter.sendMail({
     from: process.env.EMAIL_USER,
@@ -29,4 +35,6 @@ export const sendOtpEmail = async (to, otp) => {
       </div>
     `,
   });
+
+  console.log("EMAIL SENT");
 };
