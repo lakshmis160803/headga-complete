6275effef4bfe415439a67306ae9e841afa85787
Lakshmi S <lakshmistla17@gmail.com>
2026-06-04 16:24:31 +0530

    fix: brevo api update

--- SERVER/utils/sendOtp.js
+++ SERVER/utils/sendOtp.js
@@ -1,20 +1,18 @@
-import * as SibApiV3Sdk from "@getbrevo/brevo";
+import { Brevo } from "@getbrevo/brevo";
 
 export const sendOtpEmail = async (to, otp) => {
   try {
-    console.log("BREVO SDK keys:", Object.keys(SibApiV3Sdk));
-
-    const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
-    apiInstance.authentications['api-key'].apiKey = process.env.KEY;
-
-    const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
-    sendSmtpEmail.subject = "Your OTP Code";
-    sendSmtpEmail.htmlContent = `<h2>Email Verification</h2><h1>${otp}</h1>`;
-    sendSmtpEmail.sender = { email: "lakshmistla17@gmail.com", name: "Headga" };
-    sendSmtpEmail.to = [{ email: to }];
+    const client = new Brevo({ apiKey: process.env.KEY });
 
     console.log("SENDING EMAIL");
-    const info = await apiInstance.sendTransacEmail(sendSmtpEmail);
+
+    const info = await client.sendTransacEmail({
+      sender: { email: "lakshmistla17@gmail.com", name: "Headga" },
+      to: [{ email: to }],
+      subject: "Your OTP Code",
+      htmlContent: `<h2>Email Verification</h2><h1>${otp}</h1>`,
+    });
+
     console.log("EMAIL SENT", info);
   } catch (err) {
     console.error("BREVO ERROR:", err);
