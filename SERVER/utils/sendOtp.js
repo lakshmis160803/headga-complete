0f78dd35f92f85017211d85298c946b821da1e7e
Lakshmi S <lakshmistla17@gmail.com>
2026-06-04 15:27:57 +0530

    fix sendotp cpde

--- SERVER/utils/sendOtp.js
+++ SERVER/utils/sendOtp.js
@@ -1,37 +1,18 @@
 import nodemailer from "nodemailer";
 
 export const sendOtpEmail = async (to, otp) => {
-  console.log("EMAIL_USER:", process.env.EMAIL_USER);
-  console.log(
-    "EMAIL_PASS:",
-    process.env.EMAIL_PASS ? "FOUND" : "MISSING"
-  );
-
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  family: 4,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-  connectionTimeout: 10000,
-  greetingTimeout: 10000,
-  socketTimeout: 10000,
-});
-
-  try {
-    console.log("TRYING SMTP CONNECTION...");
-    await transporter.verify();
-    console.log("SMTP VERIFIED");
-  } catch (err) {
-    console.error("SMTP ERROR:", err);
-    throw err;
-  }
+  const transporter = nodemailer.createTransport({
+    host: "smtp-relay.brevo.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.BREVO_USER,
+      pass: process.env.BREVO_PASS,
+    },
+  });
 
   await transporter.sendMail({
-    from: process.env.EMAIL_USER,
+    from: process.env.BREVO_USER,
     to,
     subject: "Your OTP Code",
     html: `
