67c4618219be18cac7295a97e55d53c3f6524633
Lakshmi S <lakshmistla17@gmail.com>
2026-06-04 12:03:54 +0530

    Fix user casing

--- SERVER/controllers/adminController.js
+++ SERVER/controllers/adminController.js
@@ -1,6 +1,6 @@
 
 
-import User from "../models/User.js";
+import User from "../models/user.js";
 import Order from "../models/Order.js";
 import Product from "../models/Product.js";
 
--- SERVER/controllers/passport.js
+++ SERVER/controllers/passport.js
@@ -3,7 +3,7 @@ dotenv.config();
 
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
-import User from "../models/User.js";
+import User from "../models/user.js";
 
 passport.use(
   new GoogleStrategy(
